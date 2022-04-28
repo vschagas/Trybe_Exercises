9763a0d86b02403e762e447137ea96126063634c
valmir <valmirchagas88@gmail.com>
2022-04-27 21:37:28 -0300

    formulario1

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,10 +1,7 @@
-let buttons = document.querySelectorAll('.buttons');
-console.log(buttons);
-function disabledefault(event){
-    for (let button of buttons){
-        button.addEventListener('click',function(event){console.log('ola');
+let button = document.getElementById('button1');
+button.addEventListener('click', function (event){
+    event.preventDefault();
 
-        })
 
-    } 
-}disabledefault();
+
+})
