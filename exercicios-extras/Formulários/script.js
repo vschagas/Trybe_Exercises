c86cf8f6de21823ccc82b62855c6e8cd30c88577
valmir <valmirchagas88@gmail.com>
2022-05-05 14:09:14 -0300

    finalizando form

---
+++ exercicios-extras/Formulários/script.js
@@ -0,0 +1,12 @@
+let checkin = document.querySelector('#checkin');
+let checkout = document.querySelector('#checkout');
+let bedroom = document.querySelectorAll('.nBedroom');
+let people = document.querySelector('#nPessoas');
+let text = document.querySelector('#obs');
+let buttonOne = document.querySelector('.sendButton')
+let buttonTwo = document.querySelector('.clean')
+
+
+
+
+console.log(buttonTwo);
